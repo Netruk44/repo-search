8ce105dfe7b6ad1d939840c259c9348c49cf32bd
Daniel Perry <dperry@danieltperry.me>
2023-07-07 11:33:36 -0400

    Change default embeddings directory to exist in repository root.

--- repo_search/cli.py
+++ repo_search/cli.py
@@ -1,4 +1,5 @@
 import click
+import os
 
 @click.group()
 def main():
@@ -9,7 +10,7 @@ def main():
 @main.command(help = 'Generate a new embeddings dataset for a given repository.')
 @click.argument('dataset_name')
 @click.argument('repo_url_or_path')
-@click.option('--embeddings_dir', '-d', default = 'embeddings', help = 'Output directory for the generated embeddings.')
+@click.option('--embeddings_dir', '-d', default = None, help = 'Output directory for the generated embeddings.')
 @click.option('--verbose', '-v', is_flag = True, help = 'Print verbose output.')
 def generate(
     dataset_name,
@@ -19,6 +20,9 @@ def generate(
 
     from repo_search import generate_embeddings_for_repository
 
+    if embeddings_dir is None:
+        embeddings_dir = get_default_embeddings_dir()
+
     generate_embeddings_for_repository(dataset_name, repo_url_or_path, embeddings_dir, verbose)
 
 # 'query' command, queries a dataset for a given query.
@@ -26,7 +30,7 @@ def generate(
 @main.command(help = 'Query a dataset using natural language.')
 @click.argument('dataset_name')
 @click.argument('query')
-@click.option('--embeddings_dir', '-d', default = 'embeddings', help = 'Directory containing the embeddings for the dataset.')
+@click.option('--embeddings_dir', '-d', default = None, help = 'Directory containing the embeddings for the dataset.')
 @click.option('--verbose', '-v', is_flag = True, help = 'Print verbose output.')
 def query(
     dataset_name,
@@ -36,7 +40,15 @@ def query(
 
     from repo_search import query_embeddings
 
+    if embeddings_dir is None:
+        embeddings_dir = get_default_embeddings_dir()
+
     query_embeddings(dataset_name, query, embeddings_dir, verbose)
+
+
+def get_default_embeddings_dir():
+    # Return the 'embeddings' directory one folder up from where this file is located.
+    return os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', 'embeddings')
 
 if __name__ == '__main__':
     main()
